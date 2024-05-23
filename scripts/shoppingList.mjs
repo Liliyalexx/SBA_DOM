7e960cc8948f5019299aab5779f0e875d882e58f
LiliyaLexx <liliya.fedyurina@gmail.com>
2024-05-23 12:44:59 -0700

    README added

--- scripts/shoppingList.mjs
+++ scripts/shoppingList.mjs
@@ -3,7 +3,7 @@ var menuLinks = [
     { text: 'Amazon Fesh', href: 'https://www.amazon.com/alm/storefront?almBrandId=QW1hem9uIEZyZXNo&tag=googhydr-20&hvadid=445337001272&hvpos=&hvexid=&hvnetw=g&hvrand=1518582852317906518&hvpone=&hvptwo=&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9033313&hvtargid=kwd-306802166534&ref=pd_sl_7dhczrgnba_e' },
     { text: 'Hello Fresh', href: 'https://www.hellofresh.com/pages/meal-kit-delivery?c=1M-DS6J2QW8A&mealsize=3-2&utm_source=google&utm_medium=cpc&utm_campaign=SEA_US_google_Act_2024_Brand_10FM_tROAS_FDFL_WK17_Control&vs_campaign_id=2b8ffb2c-8245-40a7-bb5e-1350d3139098&utm_content=act_paidsearch_seabrand&&discount_comm_id=69a00013-80db-4f6e-8cdb-87ca07973397&dis_channel=sea-brand&gad_source=1&gclid=EAIaIQobChMI3ZCCqsefhgMVQd8WBR1FbgKJEAAYASAAEgKFFPD_BwE' },
     { text: 'walmart', href: 'https://www.walmart.com/search/?query=Food&adid=22222222224211171859&wmlspartner=wmtlabs&wl0=e&wl1=o&wl2=c&wl3=72774333445890&wl4=kwd-72774761268861:loc-190&wl5=111342&wl6=&wl7=&wl14=walmart%20food&veh=sem&gclid=01dec07bbf0a166db2b8a636bfe60851&gclsrc=3p.ds&msclkid=01dec07bbf0a166db2b8a636bfe60851' },
-    { text: 'LOGIN', href: '/account' },
+    { text: 'LOGIN', src: './Login/index.html', id : "login" },
   ];
   
     const mainEl = document.getElementsByTagName('main');
